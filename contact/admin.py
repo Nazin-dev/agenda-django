3a9bd2d356612ade5c680a63fce80a0906dec69e
Nailton Reis <nazin.dev@gmail.com>
2025-03-04 20:04:32 -0300

    Foi adicionado a tabela de contatos e os contatos unicos

--- contact/admin.py
+++ contact/admin.py
@@ -3,7 +3,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-  list_display = ('id', 'first_name', 'last_name', 'phone',)
+  list_display = ('id', 'first_name', 'last_name', 'phone', 'show')
+  list_editable = ('show',)
   list_display_links = ('phone',)
   ordering = ('id',)
   search_fields = ('id', 'first_name', 'last_name')
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.http import Http404
+from contact.models import Contact
 
 def index(request):
+  contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+  
+  context = {
+    'contacts': contacts
+  }
+  
   return render(
     request,
     'contact/pages/index.html',
+    context,
+  )
+
+
+def contact(request, contact_id):
+  single_contact = Contact.objects.filter(id=contact_id, show=True).first()
+  
+  if single_contact is None:
+    raise Http404()
+  
+  context = {
+    'contact': single_contact
+  }
+  
+  return render(
+    request,
+    'contact/pages/contact.html',
+    context,
   )
